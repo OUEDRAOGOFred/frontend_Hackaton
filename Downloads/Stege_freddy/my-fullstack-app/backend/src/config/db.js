a85c611929360f32821d89d03d821af12a7d4be2
Ia-premiere-promotion <ipremiere64@gmail.com>
2025-11-03 12:12:24 +0100

    Suppression dossier Téléchargements/Stage_freddy

--- Downloads/Stege_freddy/my-fullstack-app/backend/src/config/db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('learning_platform', 'root', 'Freddy1243.', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false
-});
-
-module.exports = sequelize;
--- Downloads/Stege_freddy/my-fullstack-app/backend/src/routes/calendarRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const calendarController = require('../controllers/calendarController');
-
-router.get('/integrations/google/auth', calendarController.startAuth);
-router.get('/integrations/google/callback', calendarController.handleCallback);
-router.post('/integrations/google/event', calendarController.createEvent);
-
-module.exports = router;
